eac004cacfac0b28e86a4c048826c1684b85d2bc
Renuka Prasad <renukaprasad.kb@gmail.com>
2021-12-13 15:20:36 +0530

    second push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const connect = require("./configs/db");
-const app = express();
+
+const register = require("./controllers/user.controller")
+
+const app = express();  
 app.use(express.json());
+
+app.use("/register", register);
 
 app.listen(4444, async() => {
     await connect();
